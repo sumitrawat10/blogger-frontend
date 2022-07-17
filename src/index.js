85979ca62973bcebcecb258b46d0fb58d299cfd3
vivek belwal <vivekbelwal144@gmail.com>
2022-07-17 19:51:20 +0530

    remove proxy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ContextProvider } from "./context/Context";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://the-blogger-backend.herokuapp.com/api"
+axios.defaults.headers.common["Access-Control-Allow-Origin"]="*";
+axios.defaults.headers.common["origin"]="*";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
